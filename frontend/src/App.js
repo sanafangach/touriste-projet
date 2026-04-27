0bf9e4a846b33f1edcc1301a82b320c81cee52c7
daoudi <daoudimohamed838@gmail.com>
2026-04-27 22:59:41 +0000

    modification de menu et ajouter des image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Menu from "./components/Menu/Menu";
+import Menu from "./components/accueil/Menu";
+import Home from "./components/accueil/home";
+import Card from "./components/pages/card";
+import Destination from "./components/pages/destination";
+import Languages from "./components/pages/languages";
+import Login from "./components/pages/login";
+import Saved from "./components/pages/saved";
 
 function App() {
   return (
-    <>
+    <Router>
       <Menu />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/card" element={<Card />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/languages" element={<Languages />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </Router>
   );
 }
-
-
 
 export default App;
