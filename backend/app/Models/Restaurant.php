0b395b0cc1eea72c7263809f300563d5d8bec258
daoudi <daoudimohamed838@gmail.com>
2026-05-23 12:45:46 +0000

    ajouter le contenu des module

--- backend/app/Models/Restaurant.php
+++ backend/app/Models/Restaurant.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    protected $fillable = [
+        'city_id',
+        'name',
+        'image',
+        'cuisine',
+        'rating',
+        'description',
+        'phone',
+        'opening_hours'
+    ];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
 }
